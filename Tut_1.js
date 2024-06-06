a0ba0167b43a0dbf3bc550081e0c7721f04c78e1
Nikhil Pawar <nrpawar9756@gmail.com>
2024-06-06 12:53:13 +0530

    first commit

--- Tut_1.js
+++ Tut_1.js
@@ -1,6 +1,7 @@
 console.log("hello");
+console.log("Nikhil");
+console.log("Pawar");
 console.log("world");
 console.log("world");
 console.log("world");
-console.log("world");
-console.log("world");
+console.log("hello");
