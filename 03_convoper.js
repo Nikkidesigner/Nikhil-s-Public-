a768cb1e1a15fcc98890811bac3625f6776d6461
Nikhil Pawar <nrpawar9756@gmail.com>
2024-06-06 15:08:33 +0530

    conversions

---
+++ 03_convoper.js
@@ -0,0 +1,14 @@
+let score = 33
+let score2="hello"
+// console.log(typeof 3);
+let acct = Number(score2)
+// console.log(typeof acct);
+/*
+1=> true
+"" => false
+"hitesh" => true
+*/
+let somenum=33
+let strngnum = String (somenum)
+console.log(strngnum);
+console.log(typeof strngnum);
