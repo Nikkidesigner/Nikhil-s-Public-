49d6764a752fc62a98622e44efbe56dce6d213de
Nikhil Pawar <nrpawar9756@gmail.com>
2024-06-08 17:45:28 +0530

    hi

--- 13_arrow.js
+++ 13_arrow.js
@@ -20,4 +20,10 @@ let username1="Nikhil"
 console.log(this.username1);
 console.log(this);
 }
+const addtwo = (num1,num2) =>(num1+num2) // if we do not use {}
+console.log(addtwo(3,4));
+const addtwo1 =(num1,num2)=>{
+    return num1+num2;
+}
+console.log(addtwo(5,6));
 //=============================================================
